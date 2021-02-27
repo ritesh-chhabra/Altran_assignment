9039e14b794ee45bc15eeae78b7ee9e5e4bb1e4a
Ritesh Chhabra <12890594+ritesh-chhabra@users.noreply.github.com>
2021-02-27 17:01:10 +0530

    Chnages in config

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ from flask_restful import Api
 # some input variables
 dbName = "altran"
 flaskPort = 50000
+mysqlConnection = "mysql://root:admin@localhost:3306/"
 
 
 app = Flask(__name__) # Construct Flask object
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost:3306/' + dbName # Mysql config
+app.config['SQLALCHEMY_DATABASE_URI'] = mysqlConnection + dbName # Mysql config
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # Suppressing some warnings
 db = SQLAlchemy(app) # Construct SQLAlchemy object
 api = Api(app) # Contruct API object
